c0e188f3941f53343849ed5523938c98a50fb3af
Rodri Garcia <rodrigarcia@Rodris-MacBook-Air.local>
2026-04-08 20:17:23 -0300

    correciones y uso de objeto Board, faltan pruebas

--- src/Program/Engine.cs
+++ src/Program/Engine.cs
@@ -6,58 +6,60 @@ namespace Ucu.Poo.GameOfLife
 {
     class Engine
     {
-/*        public void Rules(Board board)
+        public Board NewGeneration(Board board)
         {
+            /* No es necesario utilizarlo, si se pasa como parametro el 
             bool[,] gameBoard = board.matriz;
-            int boardWidh = board.width;
+            int boardWidth = board.width;
             int boardHeight = board.height;
+            */
 
-            Board cleanBoard = new Board();
+            Board cloneBoard = new Board(); // pasar parametros para creacion, ancho y alto
 
-            for (int x = 0; x < boardWidth; x++)
+            for (int x = 0; x < board.width; x++)
             {
-                for (int y = 0; y < boardHeight; y++)
+                for (int y = 0; y < board.height; y++)
                 {
                     int aliveNeighbors = 0;
                     for (int i = x-1; i<=x+1;i++)
                     {
                         for (int j = y-1;j<=y+1;j++)
                         {
-                            if(i>=0 && i<boardWidth && j>=0 && j < boardHeight && gameBoard[i,j])
+                            if(i>=0 && i<board.width && j>=0 && j < board.height && board.matriz[i,j])
                             {
                                 aliveNeighbors++;
                             }
                         }
                     }
-                    if(gameBoard[x,y])
+                    if(board.matriz[x,y])
                     {
                         aliveNeighbors--;
                     }
-                    if (gameBoard[x,y] && aliveNeighbors < 2)
+                    if (board.matriz[x,y] && aliveNeighbors < 2)
                     {
                         //Celula muere por baja población
-                        cloneboard[x,y] = false;
+                        cloneBoard.matriz[x,y] = false;
                     }
-                    else if (gameBoard[x,y] && aliveNeighbors > 3)
+                    else if (board.matriz[x,y] && aliveNeighbors > 3)
                     {
                         //Celula muere por sobrepoblación
-                        cloneboard[x,y] = false;
+                        cloneBoard.matriz[x,y] = false;
                     }
-                    else if (!gameBoard[x,y] && aliveNeighbors == 3)
+                    else if (!board.matriz[x,y] && aliveNeighbors == 3)
                     {
                         //Celula nace por reproducción
-                        cloneboard[x,y] = true;
+                        cloneBoard.matriz[x,y] = true;
                     }
                     else
                     {
                         //Celula mantiene el estado que tenía
-                        cloneboard[x,y] = gameBoard[x,y];
+                        cloneBoard.matriz[x,y] = board.matriz[x,y];
                     }
                 }
             }
-            gameBoard = cloneboard;
+            return board.matriz = cloneBoard.matriz;
         }
         
-*/
+
     }
 }
